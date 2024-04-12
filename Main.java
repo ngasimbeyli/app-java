dd1dadb79fc2a8d40e5e8d908e537ea4c9f6bc8b
ngasimbeyli <157435259+ngasimbeyli@users.noreply.github.com>
2024-04-12 11:01:04 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String[] names= {"Piotr", "Anna", "Jan", "Leszek"};
 
-        for (int i=1; i<16; i++) {
-            System.out.println( "i=" + i );
+        for (int i=0; i<4; i++) {
+            System.out.println( i+1 +"." + names[i] );
         }
     }
 }
